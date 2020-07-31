ed233ad9142a594d9bd1af0dc036c7b7a625ad7a
Juan Arevalo <jp.arevalo@ufl.edu>
2020-07-31 00:54:07 -0400

    Started new approach to particle drawings

--- Fireworks/fireworks.cpp
+++ Fireworks/fireworks.cpp
@@ -17,194 +17,215 @@ int range_randomInt(int low, int high){
 	return distribution(generator);
 }
 
+//struct Particle{
+//
+//	olc::vf2d pos, vel, acc;
+//	bool firework;
+//	int lifespan = 255;
+//	int hu;
+//
+//	Particle(){
+//		pos.x = 0.0f;
+//		pos.y = 0.0f;
+//		vel.x = 0.0f;
+//		vel.y = 0.0f;
+//		acc.x = 0.0f;
+//		acc.y = 0.0f;
+//		firework = false;
+//	}
+//
+//	Particle(float x_, float y_, bool fire, int h){
+//		firework = fire;
+//		hu = h;
+//		pos.x = x_;
+//		pos.y = y_;
+//
+//		if(firework){
+//			vel.x = (float)range_randomInt(-10, 10);
+//			vel.y = (float)range_randomInt(-25, -8);
+//		} else{
+//			vel.x = (float)range_randomInt(0, 360);
+//			vel.y = (float)range_randomInt(0, 360);
+//		}
+//
+//		acc.x = 0.0f;
+//		acc.y = 0.0f;
+//	}
+//
+//	Particle(olc::vf2d position , bool fire, int h){
+//		firework = fire;
+//		hu = h;
+//		pos = position;
+//
+//		if(firework){
+//			vel.x = (float)range_randomInt(-10, 10);
+//			vel.y = (float)range_randomInt(-25, -8);
+//		} else{
+//			vel.x = (float)range_randomInt(0, 360);
+//			vel.y = (float)range_randomInt(0, 360);
+//		}
+//
+//		acc.x = 0.0f;
+//		acc.y = 0.0f;
+//	}
+//
+//	void ApplyForce(olc::vf2d force){ acc += force; }
+//
+//	void Update(){
+//		vel += acc;
+//		if(!firework){
+//			vel *= 0.95f;
+//			lifespan -= 4;
+//		}
+//		pos += vel;
+//		acc *= 0.0f;
+//	}
+//	
+//	void SetVelocity(olc::vf2d velocity){ vel = velocity; }
+//
+//	bool isDone(){ return lifespan < 0;	}
+//
+//};
+//
+//struct Firework{
+//
+//	Particle *firework;
+//	bool exploded;
+//	std::vector<Particle *> parts;
+//	int hu;
+//	int width, height;
+//	olc::PixelGameEngine * gfx;
+//	int deadCount = 0;
+//
+//	Firework(){
+//		width = 256;
+//		height = 240;
+//		gfx = nullptr;
+//		hu = std::rand() % 255;
+//		firework = new Particle((float)range_randomInt(width), height, true, hu);
+//		exploded = false;
+//		parts.reserve(100);
+//	}
+//
+//	Firework(int screenWidth, int screenHeight, olc::PixelGameEngine* engine){
+//		width = screenHeight;
+//		height = screenWidth;
+//		gfx = engine;
+//		hu = std::rand() % 255;
+//		firework = new Particle((float)range_randomInt(width), height, true, hu);
+//		exploded = false;
+//		parts.reserve(100);
+//	}
+//
+//	~Firework(){
+//		for(unsigned int i = 0; i < parts.size(); i++){
+//			delete parts[i];
+//		}
+//		delete firework;
+//	}
+//
+//	void operator = (const Firework& rhs){
+//		width = rhs.width;
+//		height = rhs.height;
+//		gfx = rhs.gfx;
+//		hu = rhs.hu;
+//		firework = rhs.firework;
+//		exploded = rhs.exploded;
+//		parts = rhs.parts;
+//	}
+//
+//
+//
+//	void Explode(){
+//		for(unsigned int i = 0; i < parts.size(); i++)
+//			parts.push_back(new Particle(firework->pos, false, hu));
+//	}
+//
+//	void Update(){
+//		if(!exploded){
+//			firework->ApplyForce(gravity);
+//			firework->Update();
+//			
+//			if(firework->vel.y >= 0){
+//				exploded = true;
+//				Explode();
+//			}
+//		}
+//
+//		for(unsigned int i = 0; i < parts.size(); i++){
+//			if(parts[i] != nullptr){
+//				parts[i]->ApplyForce(gravity);
+//				parts[i]->Update();
+//				if(parts[i]->isDone())
+//					deadCount++;
+//			}
+//		}
+//	}
+//
+//	void Show(){
+//		if(!exploded){
+//			gfx->FillCircle(firework->pos, 2);
+//		}
+//
+//		for(unsigned int i = 0; i < parts.size(); i++){
+//			if(parts[i] != nullptr && !parts[i]->isDone()){
+//				gfx->FillCircle(parts[i]->pos, 1);
+//			}
+//		}
+//	}
+//
+//	bool isDone(){
+//		return(exploded && (deadCount == parts.size()));
+//	}
+//
+//
+//};
+
 struct Particle{
-
-	olc::vf2d pos, vel, acc;
-	bool firework;
-	int lifespan = 255;
-	int hu;
-
-	Particle(){
-		pos.x = 0.0f;
-		pos.y = 0.0f;
-		vel.x = 0.0f;
-		vel.y = 0.0f;
-		acc.x = 0.0f;
-		acc.y = 0.0f;
-		firework = false;
-	}
-
-	Particle(float x_, float y_, bool fire, int h){
-		firework = fire;
-		hu = h;
-		pos.x = x_;
-		pos.y = y_;
-
-		if(firework){
-			vel.x = (float)range_randomInt(-10, 10);
-			vel.y = (float)range_randomInt(-25, -8);
-		} else{
-			vel.x = (float)range_randomInt(0, 360);
-			vel.y = (float)range_randomInt(0, 360);
-		}
-
-		acc.x = 0.0f;
-		acc.y = 0.0f;
-	}
-
-	Particle(olc::vf2d position , bool fire, int h){
-		firework = fire;
-		hu = h;
-		pos = position;
-
-		if(firework){
-			vel.x = (float)range_randomInt(-10, 10);
-			vel.y = (float)range_randomInt(-25, -8);
-		} else{
-			vel.x = (float)range_randomInt(0, 360);
-			vel.y = (float)range_randomInt(0, 360);
-		}
-
-		acc.x = 0.0f;
-		acc.y = 0.0f;
-	}
-
-	void ApplyForce(olc::vf2d force){ acc += force; }
-
-	void Update(){
-		vel += acc;
-		if(!firework){
-			vel *= 0.95f;
-			lifespan -= 4;
-		}
-		pos += vel;
-		acc *= 0.0f;
-	}
-	
-	void SetVelocity(olc::vf2d velocity){ vel = velocity; }
-
-	bool isDone(){ return lifespan < 0;	}
-
-};
-
-struct Firework{
-
-	Particle *firework;
-	bool exploded;
-	std::vector<Particle *> parts;
-	int hu;
-	int width, height;
-	olc::PixelGameEngine * gfx;
-	int deadCount = 0;
-
-	Firework(){
-		width = 256;
-		height = 240;
-		gfx = nullptr;
-		hu = std::rand() % 255;
-		firework = new Particle((float)range_randomInt(width), height, true, hu);
-		exploded = false;
-		parts.reserve(100);
-	}
-
-	Firework(int screenWidth, int screenHeight, olc::PixelGameEngine* engine){
-		width = screenHeight;
-		height = screenWidth;
-		gfx = engine;
-		hu = std::rand() % 255;
-		firework = new Particle((float)range_randomInt(width), height, true, hu);
-		exploded = false;
-		parts.reserve(100);
-	}
-
-	~Firework(){
-		for(unsigned int i = 0; i < parts.size(); i++){
-			delete parts[i];
-		}
-		delete firework;
-	}
-
-	void operator = (const Firework& rhs){
-		width = rhs.width;
-		height = rhs.height;
-		gfx = rhs.gfx;
-		hu = rhs.hu;
-		firework = rhs.firework;
-		exploded = rhs.exploded;
-		parts = rhs.parts;
-	}
-
-
-
-	void Explode(){
-		for(unsigned int i = 0; i < parts.size(); i++)
-			parts.push_back(new Particle(firework->pos, false, hu));
-	}
-
-	void Update(){
-		if(!exploded){
-			firework->ApplyForce(gravity);
-			firework->Update();
-			
-			if(firework->vel.y >= 0){
-				exploded = true;
-				Explode();
-			}
-		}
-
-		for(unsigned int i = 0; i < parts.size(); i++){
-			if(parts[i] != nullptr){
-				parts[i]->ApplyForce(gravity);
-				parts[i]->Update();
-				if(parts[i]->isDone())
-					deadCount++;
-			}
-		}
-	}
-
-	void Show(){
-		if(!exploded){
-			gfx->FillCircle(firework->pos, 2);
-		}
-
-		for(unsigned int i = 0; i < parts.size(); i++){
-			if(parts[i] != nullptr && !parts[i]->isDone()){
-				gfx->FillCircle(parts[i]->pos, 1);
-			}
-		}
-	}
-
-	bool isDone(){
-		return(exploded && (deadCount == parts.size()));
-	}
-
-
+	olc::vi2d pos, vel, acc;
 };
 
 // Override base class with your custom functionality
 class Fireworks : public olc::PixelGameEngine{
+public:
+	Particle firework;
+	olc::vi2d gravity = {0, 20};
+	float targetFrameTime = 1.00f / 60.0f;
+	float accumulatedTime = 0.00f;
+
 public:
 	Fireworks(){
 		// Name you application
 		sAppName = "Fireworks";
 	}
 
-	Firework fr;
-
 public:
 	bool OnUserCreate() override{
 		// Called once at the start, so create things here
-		fr = Firework(ScreenWidth(), ScreenHeight(), this);
-		gravity.x = 0.0f;
-		gravity.y = 0.2f;
+		firework.pos.x = ScreenWidth() / 2;
+		firework.pos.y = ScreenHeight();
+		firework.vel.y = -500;
 		return true;
 	}
 
+	void DrawParticle(Particle& part){
+		FillCircle(part.pos, 10, olc::RED);
+	}
+
+	void UpdateParticle(Particle& part, float fElapsedTime){
+		part.acc += gravity;
+		part.vel += part.acc * fElapsedTime;
+		part.pos += part.vel * fElapsedTime;
+
+		part.acc *= 0;
+	}
+
 	bool OnUserUpdate(float fElapsedTime) override{
-		// called once per frame, draws random coloured pixels
-		Clear(olc::Pixel(0, 0, 0, 25));
-		fr.Show();
-		fr.Update();
+		//Clear(olc::Pixel(0, 0, 0, 25));
+		Clear(olc::BLACK);
+		UpdateParticle(firework, fElapsedTime);
+		DrawParticle(firework);
+
 
 
 		return true;
@@ -213,7 +234,7 @@ public:
 
 int main(){
 	Fireworks demo;
-	if(demo.Construct(400, 300, 1, 1))
+	if(demo.Construct(400, 300, 2, 2, false, true))
 		demo.Start();
 	return 0;
 }
