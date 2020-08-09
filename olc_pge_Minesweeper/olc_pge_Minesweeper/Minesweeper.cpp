20f7b5ba807a4613bcab51f83de9c34164c3591d
Juan Arevalo <jp.arevalo@ufl.edu>
2020-08-08 20:57:04 -0400

    Finished the game :D

--- olc_pge_Minesweeper/olc_pge_Minesweeper/Minesweeper.cpp
+++ olc_pge_Minesweeper/olc_pge_Minesweeper/Minesweeper.cpp
@@ -4,6 +4,7 @@
 
 
 
+
 struct Tile{
 	int x, y;
 	bool bomb = false;
@@ -32,9 +33,16 @@ struct Tile{
 class Minesweeper : public olc::PixelGameEngine{
 private:
 	int rows, cols, numMines;
+	int flags = 0;
+	int numOpenCells = 0;
 	int tileWidth;
 	int tileHeight;
 	bool debug = false;
+	bool playing = true;
+	bool gameLost = false;
+
+	int numMinesDrawn = 0;
+	
 	
 	std::vector<Tile> board;
 
@@ -49,12 +57,28 @@ public:
 	}
 
 	void RestartGame(){
+		playing = true;
+		gameLost = false;
+		numOpenCells = 0;
 		for(int i = 0; i < board.size(); i++){
 			board[i].open = false;
 			board[i].bomb = false;
+			board[i].flagged = false;
 		}
 
+		flags = 0;
+
 		FillMines();
+	}
+
+	void DrawStatusBar(){
+		int minesLeft = numMines - flags;
+		std::string mines = "Mines: " + std::to_string(minesLeft);
+		DrawString(10, ScreenHeight() - 20, mines, olc::RED, 1);
+
+		FillRect(ScreenWidth() - 80, ScreenHeight() - 30, 70, 29, olc::BLUE);
+		DrawRect(ScreenWidth() - 80, ScreenHeight() - 30, 70, 29, olc::VERY_DARK_BLUE);
+		DrawString(ScreenWidth() - 72, ScreenHeight() - 20, "Restart", olc::RED, 1);
 	}
 
 	void DrawBoard(){
@@ -89,7 +113,7 @@ public:
 			int count = 0;
 			for(int x = -1; x <= 1; x++)
 				for(int y = -1; y <= 1; y++)
-					if((i + (x * cols + y)) >= 0 && (i + (x * cols + y)) < board.size())
+					if((board[i].x + x) >= 0 && (board[i].x + x) < cols && (board[i].y + y) >= 0 && (board[i].y + y) < rows)
 						if(board[i + (x * cols + y)].bomb) count++;
 
 			board[i].numNeihbors = count;
@@ -113,12 +137,19 @@ public:
 
 	void OpenCells(Tile& t){
 		if(!t.open && !t.flagged){
-			t.open = true;
-			if(t.numNeihbors == 0){
-				for(int x = -1; x <= 1; x++)
-					for(int y = -1; y <= 1; y++)
-						if((t.x + x) * cols + (t.y + y) >= 0 && (t.x + x) * cols + (t.y + y) < board.size())
-							OpenCells(board[(t.x + x) * cols + (t.y + y)]);
+			if(!t.bomb){
+				t.open = true;
+				numOpenCells++;
+				if(t.numNeihbors == 0){
+					for(int x = -1; x <= 1; x++)
+						for(int y = -1; y <= 1; y++)
+							if((t.x + x) >= 0 && (t.x + x) < cols && (t.y + y) >= 0 && (t.y + y) < rows)
+								OpenCells(board[(t.x + x) * cols + (t.y + y)]);
+				}
+			} else{
+				t.open = true;
+				playing = false;
+				gameLost = true;
 			}
 		}
 	}
@@ -127,8 +158,9 @@ public:
 	bool OnUserCreate() override{
 
 		tileWidth = ScreenWidth() / cols;
-		tileHeight = ScreenHeight() / rows;
+		tileHeight = (ScreenHeight() - 30) / rows;
 
+		
 		for(int x = 0; x < cols; x++){
 			for(int y = 0; y < rows; y++){
 				board.push_back(Tile(x, y));
@@ -145,28 +177,46 @@ public:
 
 	bool OnUserUpdate(float elapsedTime) override{
 
+		Clear(olc::BLACK);
+
+		// Check win
+		if((rows * cols - numOpenCells) == numMines) playing = false;
+
 		if(GetKey(olc::SPACE).bPressed) debug = !debug;
 
 		if(GetKey(olc::R).bPressed) RestartGame();
 
 		if(GetMouse(0).bReleased){
-			int x = GetMouseX() / tileWidth;
-			int y = GetMouseY() / tileHeight;
-			std::cout << "pressed the (" << x << ", " << y << ") tile with " << board[x * cols + y].numNeihbors << std::endl;
-			
-			OpenCells(board[x * cols + y]);
+			if(GetMouseY() < ScreenHeight() - 30 && playing){
+				int x = GetMouseX() / tileWidth;
+				int y = GetMouseY() / tileHeight;
+				std::cout << "pressed the (" << x << ", " << y << ") tile with " << board[x * cols + y].numNeihbors << std::endl;
+
+				OpenCells(board[x * cols + y]);				
+			}//FillRect(ScreenWidth() - 80, ScreenHeight() - 30, 70, 29, olc::BLUE);
+			else if(GetMouseY() > ScreenHeight() - 30 && GetMouseX() > (ScreenWidth() - 80) && GetMouseX() < (ScreenWidth() - 10) ){
+				RestartGame();
+			}
 		}
 
-		if(GetMouse(1).bReleased){
+		if(GetMouse(1).bReleased && GetMouseY() < ScreenHeight() - 30){
 			int x = GetMouseX() / tileWidth;
 			int y = GetMouseY() / tileHeight;
 			//std::cout << "pressed the (" << x << ", " << y << ") tile with " << board[x * cols + y].numNeihbors << std::endl;
-			if(!board[x * cols + y].open)
-				board[x * cols + y].flagged = !board[x * cols + y].flagged;
+			if(!board[x * cols + y].open && !board[x * cols + y].flagged){
+				board[x * cols + y].flagged = true;
+				flags++;
+			} else if(board[x * cols + y].flagged){
+				board[x * cols + y].flagged = false;
+				flags--;
+			}
 		}
 
-		
 		DrawBoard();
+		DrawStatusBar();
+		if(!playing)
+			if(gameLost) DrawString(ScreenWidth() / 2 - 40, ScreenHeight() - 20, "Game Over :(", olc::RED);
+			else if(!gameLost) DrawString(ScreenWidth() / 2 - 40, ScreenHeight() - 20, "Game Over! :D", olc::RED);
 		return true;
 	}
 
@@ -180,7 +230,7 @@ public:
 int main(){
 
 	Minesweeper game(10, 10, 10);
-	if(game.Construct(401, 301, 2, 2))
+	if(game.Construct(401, 331, 2, 2))
 		game.Start();
 
 	return 0;
